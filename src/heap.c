fc0bb6baa55b652d2ce15e4925b877fce33c54e0
Erikrizk123 <erikrizk2802@gmail.com>
2022-09-19 20:28:57 -0400

    Stores data, alters data after free, calls print if destroy is called before all memory is freed. It compiles but doesn't print.

--- src/heap.c
+++ src/heap.c
@@ -15,12 +15,23 @@ typedef struct arena_t
 	struct arena_t* next;
 } arena_t;
 
+// Stores backtrace, address of memory, size of individual memory, ptr to next storage
+typedef struct storage_t
+{
+	void** backtrace;
+	void* address;
+	size_t size;
+	struct storage_t* s_next;
+} storage_t;
+
 typedef struct heap_t
 {
 	tlsf_t tlsf;
 	size_t grow_increment;
 	arena_t* arena;
+	struct storage_t* s_next; // ptr to first memory alloc data
 } heap_t;
+
 
 heap_t* heap_create(size_t grow_increment)
 {
@@ -43,7 +54,7 @@ heap_t* heap_create(size_t grow_increment)
 
 void* heap_alloc(heap_t* heap, size_t size, size_t alignment)
 {
-	void* address = tlsf_memalign(heap->tlsf, alignment, size);
+	void* address = tlsf_memalign(heap->tlsf, alignment, size + sizeof(storage_t));
 	if (!address)
 	{
 		size_t arena_size =
@@ -65,7 +76,27 @@ void* heap_alloc(heap_t* heap, size_t size, size_t alignment)
 		arena->next = heap->arena;
 		heap->arena = arena;
 
-		address = tlsf_memalign(heap->tlsf, alignment, size);
+		address = tlsf_memalign(heap->tlsf, alignment, size + sizeof(storage_t));
+	}
+	if (address) {
+		// Adds data to the struct
+		storage_t* storage = VirtualAlloc(NULL, sizeof(storage_t),
+			MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+		storage->address = address;
+		debug_backtrace(storage->backtrace, 8);
+		storage->size = size;
+
+		// Connects the ptr of last added memory data to current memory data
+		if (heap->s_next) {
+			storage_t* cur = heap->s_next;
+			while (cur->s_next) {
+				cur->s_next = cur->s_next;
+			}
+			cur->s_next = storage;
+		}
+		else {
+			heap->s_next = storage;
+		}
 	}
 	return address;
 }
@@ -73,10 +104,48 @@ void* heap_alloc(heap_t* heap, size_t size, size_t alignment)
 void heap_free(heap_t* heap, void* address)
 {
 	tlsf_free(heap->tlsf, address);
+	// finds which memory storage matches the address
+	storage_t* prev = heap->s_next;
+	storage_t* cur = heap->s_next;
+	while (cur->address != address) {
+		prev = cur;
+		cur = cur->s_next;
+	}
+
+	// fixes ptrs
+	if (cur->s_next == NULL) {
+		if (prev == NULL) {
+			heap->s_next = NULL;
+		}
+		else {
+			prev->s_next = NULL;
+		}
+	}
+	else {
+		if (prev == NULL) {
+			heap->s_next = cur->s_next;
+		}
+		else {
+			prev->s_next = cur->s_next;
+		}
+	}
 }
 
 void heap_destroy(heap_t* heap)
 {
+	// if there is still unfreed memory
+	if (heap->s_next) {
+		storage_t* cur = heap->s_next;
+		while (cur != NULL) {
+			debug_print(
+				k_print_info,
+				"Memory leak of size %zu bytes with callstack:\n",
+				cur->size,
+				cur->backtrace);
+			cur = cur->s_next;
+		}
+	}
+
 	tlsf_destroy(heap->tlsf);
 
 	arena_t* arena = heap->arena;
